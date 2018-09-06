de8f1cea469b4ed9229591e0e9e8096a2dfb8d7e
jinuman <jin35kim@gmail.com>
2018-09-06 20:00:52 +0900

    Implement EntryViewController base

---
+++ Journal/Sources/Controllers/EntryViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Journal
+//
+//  Created by 김진우 on 2018. 8. 13..
+//  Copyright © 2018년 jinuman. All rights reserved.
+//
+
+import UIKit
+
+extension DateFormatter {
+    static var entryDateFormatter: DateFormatter = { () -> DateFormatter in
+        let df = DateFormatter()
+        df.dateFormat = "yyyy. MM. dd. EEE"
+        return df
+    }()
+}
+
+class EntryViewController: UIViewController {
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var textView: UITextView!
+    
+    let journal: Journal = InMemoryJournal()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        dateLabel.text = DateFormatter.entryDateFormatter.string(from: Date())
+        textView.text = "Journal text test"
+    }
+
+    @IBAction func saveEntry(_ sender: Any) {
+        let entry: Entry = Entry(text: textView.text)
+        journal.add(entry)
+        
+        print("Entry 개수: ", journal.recentEntries(max: Int.max).count)
+    }
+}
+
