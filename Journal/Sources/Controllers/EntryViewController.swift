d274513984d5da0a1820e69357e85ca6a4955052
jinuman <jin35kim@gmail.com>
2018-09-09 15:57:18 +0900

    Implement edit Entry using target-action pattern

--- Journal/Sources/Controllers/EntryViewController.swift
+++ Journal/Sources/Controllers/EntryViewController.swift
@@ -19,13 +19,18 @@ extension DateFormatter {
 class EntryViewController: UIViewController {
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var button: UIButton!
     
     let journal: Journal = InMemoryJournal()
+    private var editingEntry: Entry?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         dateLabel.text = DateFormatter.entryDateFormatter.string(from: Date())
         textView.text = "Journal text test"
+        button.addTarget(self,
+                         action: #selector(saveEntry(_:)),
+                         for: UIControlEvents.touchUpInside)
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -33,12 +38,42 @@ class EntryViewController: UIViewController {
         textView.becomeFirstResponder()
     }
 
-    @IBAction func saveEntry(_ sender: Any) {
-        let entry: Entry = Entry(text: textView.text)
-        journal.add(entry)
-        
-        textView.isEditable = false
-        textView.resignFirstResponder()
+    @objc func saveEntry(_ sender: Any) {
+        if let editing = editingEntry {
+            editing.text = textView.text
+            journal.update(editing)
+            print("editing text save event")
+        } else {
+            let entry: Entry = Entry(text: textView.text)
+            journal.add(entry)
+            editingEntry = entry
+            print("first text save event")
+        }
+        updateSubviews(for: false)
+    }
+    
+    @objc func editEntry(_ sender: Any) {
+        updateSubviews(for: true)
+    }
+    
+    fileprivate func updateSubviews(for isEditing: Bool) {
+        if isEditing {
+            textView.isEditable = true
+            textView.becomeFirstResponder()
+            
+            button.setTitle("저장하기", for: UIControlState.normal)
+            button.addTarget(self,
+                             action: #selector(saveEntry(_:)),
+                             for: UIControlEvents.touchUpInside)
+        } else {
+            textView.isEditable = false
+            textView.resignFirstResponder()
+            
+            button.setTitle("수정하기", for: UIControlState.normal)
+            button.addTarget(self,
+                             action: #selector(editEntry(_:)),
+                             for: UIControlEvents.touchUpInside)
+        }
     }
 }
 
